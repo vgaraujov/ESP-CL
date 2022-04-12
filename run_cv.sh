f94a97a060469a110951f628b24082f29cfa887c
Julio <julious.hurtado@gmail.com>
2022-04-12 09:47:42 -0400

    Update run_cv.sh

--- run_cv.sh
+++ run_cv.sh
@@ -2,8 +2,10 @@ cd CV
 
 """
 --use_highway To use a model with branch layers
+--pretrained Use a pre-trained ResNet
+--train_highway_only To use only the memory to train the branch classifiers, alternative is to use only data from the current task
 --mem_per_tasks Amount of element per task to save in memory
 --only_mem To use only the memory to train the model
 """
 
-python train.py --use_highway --mem_per_tasks 100 --only_mem
+python train.py --use_highway --pretrained --train_highway_only --mem_per_tasks 100 --only_mem
